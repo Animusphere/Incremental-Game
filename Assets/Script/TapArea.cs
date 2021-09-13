2fa1af3ed8c3eb90b0dd8b51ed738a49ead02265
Ricky Limbaug <54813543+Animusphere@users.noreply.github.com>
2021-09-13 18:02:37 +0700

    Initial commit

---
+++ Assets/Script/TapArea.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour, IPointerDownHandler
+{
+    private void Start() { }
+    private void Update() { }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+    }
+}
